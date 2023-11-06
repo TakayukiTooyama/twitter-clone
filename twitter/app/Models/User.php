f709b5f1bf401b93751d1706596c8615ebf8d98b
Takayuki Tooyama <t.tooyama107@gmail.com>
2023-11-06 17:28:30 +0900

    ✨ ユーザー一覧表示

--- twitter/app/Models/User.php
+++ twitter/app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Retrieve all users from the database.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public static function getAll()
+    {
+        return static::all();
+    }
 }
