93fb7a09cc6593423ecce16a0a06b2e06ed555d5
Takayuki Tooyama <t.tooyama107@gmail.com>
2023-11-07 13:38:56 +0900

    ♻️ useでのパス指定をA-Z順に並び替え

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
