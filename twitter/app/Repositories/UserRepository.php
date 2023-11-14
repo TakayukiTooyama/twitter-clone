89c52a14dffcd63e88bd05639cb82cbe846234d8
Takayuki Tooyama <t.tooyama107@gmail.com>
2023-11-14 17:45:38 +0900

    🎨 ユーザー削除処理をRepository層とService層へ

--- twitter/app/Repositories/UserRepository.php
+++ twitter/app/Repositories/UserRepository.php
@@ -41,4 +41,15 @@ class UserRepository
     {
         User::where('id', $userId)->update($userInfo);
     }
+    /**
+     * ユーザー削除
+     *
+     * @param int $userId
+     *
+     * @return bool
+     */
+    public function delete(int $userId): bool
+    {
+        return User::where('id', $userId)->delete();
+    }
 }
