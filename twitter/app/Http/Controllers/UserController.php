d6d4b2e4e537d3eaa65bfdd5e7224f5e649f2db9
Takayuki Tooyama <t.tooyama107@gmail.com>
2023-11-14 17:11:47 +0900

    🎨 Service層とRepository層を追加しロジックを分離

--- twitter/app/Http/Controllers/UserController.php
+++ twitter/app/Http/Controllers/UserController.php
@@ -3,33 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserUpdateRequest;
-use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class UserController extends Controller
 {
+    protected $userService;
 
-    /**
-     * Userモデルのインスタンス
-     *
-     * @var User
-     */
-    protected $user;
-
-    /**
-     * 新しいコントローラインスタンスの生成
-     *
-     * @param User $user
-     *
-     * @return void
-     */
-    public function __construct(User $user)
+    public function __construct(UserService $userService)
     {
-        $this->user = $user;
+        $this->userService = $userService;
     }
+
 
     /**
      * ユーザー一覧を取得し、表示する
@@ -38,7 +25,7 @@ class UserController extends Controller
      */
     public function index(): View
     {
-        $users = $this->user->getAll();
+        $users = $this->userService->getAllUser();
         return view('user.index', compact('users'));
     }
 
@@ -51,7 +38,7 @@ class UserController extends Controller
      */
     public function show(int $userId): View|RedirectResponse
     {
-        $user = $this->user->findByUserId($userId);
+        $user = $this->userService->findUserById($userId);
         if (!$user) {
             return redirect()->route('users.index')->with('error', 'User not found');
         }
@@ -61,27 +48,18 @@ class UserController extends Controller
     /**
      * ユーザー情報を更新する
      *
-     * @param Request $request
+     * @param UserUpdateRequest $request
      *
      * @return RedirectResponse
      */
     public function update(UserUpdateRequest $request): RedirectResponse
     {
-        // リクエストの取得
-        $validatedUserInfo = $request->only(['name', 'email', 'password']);
+        try {
+            $this->userService->updateUserInfo(Auth::id(), $request->validated());
 
-        // パスワードが提供されている場合のみ更新、そうでなければ配列から削除
-        if (!empty($validatedUserInfo['password'])) {
-            $validatedUserInfo['password'] = Hash::make($validatedUserInfo['password']);
-        } else {
-            unset($validatedUserInfo['password']);
+            return back()->with('success', 'ユーザー情報を更新しました。');
+        } catch (\Exception $e) {
+            return back()->with('error', 'ユーザー情報の更新に失敗しました: ' . $e->getMessage());
         }
-
-        // ユーザー情報の更新
-        $isUpdated = $this->user->userInfoUpdate($validatedUserInfo);
-
-        return $isUpdated
-            ? back()->with('success', 'ユーザー情報を更新しました。')
-            : back()->with('error', 'ユーザー情報に失敗しました。');
     }
 }
---
+++ twitter/app/Repositories/UserRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserRepository
+{
+    /**
+     * ユーザー一覧取得
+     *
+     * @return Collection
+     */
+    public function findAll(): Collection
+    {
+        return User::all();
+    }
+
+    /**
+     * ユーザーIDからユーザー情報を取得
+     *
+     * @param int $userId
+     *
+     * @return User|null
+     */
+    public function findById(int $userId): ?User
+    {
+        return User::find($userId);
+    }
+
+    /**
+     * ユーザー情報を更新
+     *
+     * @param int $userId
+     * @param array $userInfo
+     *
+     * @return void
+     */
+    public function update(int $userId, array $userInfo): void
+    {
+        User::where('id', $userId)->update($userInfo);
+    }
+}
